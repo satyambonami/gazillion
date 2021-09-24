d164116e795bfede2f9817d6ea29b9e81cd7012d
satyambonami <satyambonami@bonamisoftware.com>
2021-09-24 15:28:06 +0530

    blog changes done

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -128,7 +128,35 @@ $('.slide3').slick({
   slideToScroll: 1,
   autoplay: false,
   centerMode: true,
-  variableWidth: true,
+  variableWidth:true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true,
+        variableWidth:false
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        variableWidth:false,
+        centerPadding: '100px',
+      }
+    },
+  ]
 });
 
 $('.video-marketing').slick({
